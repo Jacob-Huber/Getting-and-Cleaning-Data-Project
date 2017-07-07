016921b73101eb59c6b33c1c46c368a6375f8354
Jacob Huber <jthuber93@gmail.com>
2017-07-07 14:20:28 -0400

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -3,9 +3,8 @@
 ## training and test were data tables of the files X_train.txt and X_test.txt
 ## I used the read.table function to get each into its data table
 library(dplyr)
-i <- (c(0,1,2,40,41,42,80,81,82))
 
 
 
 gyro <- merge(training, test)
-
+write.table(gyro, file = "Run_tidy.txt", row.name = FALSE)
